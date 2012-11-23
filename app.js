1fbec3cdef68322143cda3687754036c5ba0d155
Zach Gohr <zachary.gohr@gmail.com>
2012-11-23 15:24:50 -0600

    Removed defunct compile function removed from Express some time ago.

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  //app.use(express.compiler({ src: __dirname + '/public', enable: ['less']}));
   app.use(lessMiddleware({
     src: __dirname + '/public',
     compress: true
